c132562ea0a84828cad80442da13773d6f71135b
Jonathan Bravo <jonathan.a.bravo@gmail.com>
2020-02-24 16:10:51 -0500

    Changing entity name to User, previously Auth

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const mongodbConnection = require("../dbconfig/connection.js");
 const user = {
     getUser: (data, cb) => {
-        const collection = mongodbConnection.db().collection("Auth");
+        const collection = mongodbConnection.db().collection("User");
         collection.findOne({ email: data.email }, (findError, findResult) => {
             if(findResult){
                 const {password, _id, ...rest} = findResult;
