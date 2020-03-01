440af0111e640f1daf4f72644c8c8ce96ea46cbe
Dominic Bloomfield <dbloomfield@tracfone.com>
2020-03-01 03:04:30 -0500

    Update WishList.js

--- client/src/components/WishList.js
+++ client/src/components/WishList.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import API from "../utils/API";
-import { Form, Alert, Button, Container, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, FormGroup, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, ListGroup, ListGroupItem, Badge } from "reactstrap";
+import { Form, Alert, Button, Container, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, FormGroup, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, ListGroup, ListGroupItem, Badge, UncontrolledCollapse } from "reactstrap";
 
 const WishList = (props) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -43,6 +43,8 @@ const WishList = (props) => {
              <DropdownItem>Horror</DropdownItem>
              <DropdownItem>Self-Help</DropdownItem>
              <DropdownItem>Biography</DropdownItem>
+             <DropdownItem divider />
+             <DropdownItem onClick = {toggle}>New List</DropdownItem>
            </DropdownMenu>
          </Dropdown>
     </Form>
@@ -51,8 +53,7 @@ const WishList = (props) => {
 
 
 
-          <Button type="Submit" size = "sm" style = {{marginTop: "5px"} } onClick={toggle}>New List</Button>
-          <a href ="#" style = {{paddingLeft: "10px"}}>View Lists</a>
+          <a href ="#" id = "toggler" style = {{paddingLeft: "5px", paddingTop: "10px"}}>View Lists</a>
         </div>
 
         <div>
@@ -66,19 +67,60 @@ const WishList = (props) => {
                     <Button color="primary" onClick={toggle}>Create List</Button>{' '}
                     <Button color="secondary" onClick={toggle}>Cancel</Button>
                 </ModalFooter>
+                </Modal>
 
         </div>
+
 
         <div>
-          <ListGroup flush>
-            <ListGroupItem tag = "a" href = "#">Fiction<Badge pill style = {{marginLeft: "2%"}}>6</Badge></ListGroupItem>
-            <ListGroupItem tag = "a" href = "#">Horror<Badge pill style = {{marginLeft: "2%"}}>3</Badge></ListGroupItem>
-            <ListGroupItem tag = "a" href = "#">Self-Help<Badge pill style = {{marginLeft: "2%"}}>1</Badge></ListGroupItem>
-            <ListGroupItem tag = "a" href = "#">Biography<Badge pill style = {{marginLeft: "2%"}}>5</Badge></ListGroupItem>
-          </ListGroup>
+        <UncontrolledCollapse toggler = "#toggler">
+          <ListGroup flush style = {{marginRight: "75%"}}>
+            <ListGroupItem tag = "a" href = "#" id="toggle-fiction">Fiction<Badge pill style = {{marginLeft: "2%"}}>6</Badge></ListGroupItem>
+              <UncontrolledCollapse toggler = "#toggle-fiction">
 
+                  <p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>Of Mice and Men</p>
+                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Maximum Ride</p>
+                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Game of Thrones</p>
+                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>God is Dead</p>
+                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Epitomes of Conviction</p>
+                  <p style = {{marginLeft: "10%", marginBottom: "2%"}}>Hell Buried</p>
+
+              </UncontrolledCollapse>
+            <ListGroupItem tag = "a" href = "#" id = "toggle-horror">Horror<Badge pill style = {{marginLeft: "2%"}}>3</Badge></ListGroupItem>
+
+              <UncontrolledCollapse toggler = "#toggle-horror">
+
+                  <p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>The Shining</p>
+                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Scratches</p>
+                  <p style = {{marginLeft: "10%", marginBottom: "2%"}}>The Hostile Hospital</p>
+
+
+              </UncontrolledCollapse>
+            <ListGroupItem tag = "a" href = "#" id = "toggle-SH">Self-Help<Badge pill style = {{marginLeft: "2%"}}>1</Badge></ListGroupItem>
+              <UncontrolledCollapse toggler = "#toggle-SH">
+
+                  <p style = {{marginLeft: "10%", marginBottom: "2%", marginTop: "2%"}}>How To Be A Badass</p>
+
+
+
+              </UncontrolledCollapse>
+            <ListGroupItem tag = "a" href = "#" id = "toggle-bio">Biography<Badge pill style = {{marginLeft: "2%"}}>5</Badge></ListGroupItem>
+
+            <UncontrolledCollapse toggler = "#toggle-bio">
+
+                <p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>Slash: A Rockstars Guide to Life</p>
+                <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Some Book About Oprah</p>
+                <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Morgan Freeman Has A Cool Voice: Heres Why</p>
+                <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Obama? Obama.</p>
+                <p style = {{marginLeft: "10%", marginBottom: "0px"}}>A Day In The Life of a Wendy's Employee</p>
+
+            </UncontrolledCollapse>
+
+          </ListGroup>
+          </UncontrolledCollapse>
 
         </div>
+
       </div>
 
 
