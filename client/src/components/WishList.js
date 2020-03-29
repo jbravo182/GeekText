7ea2ec570a5eac795bbe4b350c5eccd78f19539a
Dominic Bloomfield <DBLOOMFIELD@TRACFONE.COM>
2020-03-29 03:06:07 -0400

    Added delete and add-to-cart icons
    Added icons to remove an item from a list or a list altogether, as well as an icon to add item to a shopping cart. No functionality added thus far.

--- client/src/components/WishList.js
+++ client/src/components/WishList.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import API from "../utils/API";
 import { Form, Alert, Button, Container, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, FormGroup, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, ListGroup, ListGroupItem, Badge, UncontrolledCollapse } from "reactstrap";
+import { MongoClient } from 'mongodb'
+import axios from 'axios'
+
+
+
 
 const WishList = (props) => {
+
+
+
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggle2 = () => setDropdownOpen(prevState => !prevState);
 
@@ -14,12 +22,18 @@ const WishList = (props) => {
   const [modal, setModal] = useState(false);
   const [unmountOnClose, setUnmountOnClose] = useState(true);
 
+
+
     const toggle = () => setModal(!modal);
     const changeUnmountOnClose = e => {
         let value = e.target.value;
         setUnmountOnClose(JSON.parse(value));
 
   }
+
+
+
+
 
   return(
 
@@ -75,44 +89,116 @@ const WishList = (props) => {
         <div>
         <UncontrolledCollapse toggler = "#toggler">
           <ListGroup flush style = {{marginRight: "75%"}}>
-            <ListGroupItem tag = "a" href = "#" id="toggle-fiction">Fiction<Badge pill style = {{marginLeft: "2%"}}>6</Badge></ListGroupItem>
+            <ListGroupItem tag = "a" href = "#" id="toggle-fiction">Fiction<Badge pill style = {{marginLeft: "2%"}}>6</Badge><svg class="bi bi-trash" style = {{marginLeft: "50%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/>
+</svg></ListGroupItem>
               <UncontrolledCollapse toggler = "#toggle-fiction">
 
-                  <p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>Of Mice and Men</p>
-                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Maximum Ride</p>
-                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Game of Thrones</p>
-                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>God is Dead</p>
-                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Epitomes of Conviction</p>
-                  <p style = {{marginLeft: "10%", marginBottom: "2%"}}>Hell Buried</p>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>Of Mice and Men <a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>Maximum Ride <a href ="#"style = {{marginLeft: "2%"}} >Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>Game of Thrones <a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>God is Dead <a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>Epitomes of Conviction <a href ="#" style = {{marginLeft: "2%"}} >Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "2%"}}>Hell Buried <a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
 
               </UncontrolledCollapse>
-            <ListGroupItem tag = "a" href = "#" id = "toggle-horror">Horror<Badge pill style = {{marginLeft: "2%"}}>3</Badge></ListGroupItem>
+            <ListGroupItem tag = "a" href = "#" id = "toggle-horror">Horror<Badge pill style = {{marginLeft: "2%"}}>3</Badge><svg class="bi bi-trash" style = {{marginLeft: "50%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/>
+</svg></ListGroupItem>
 
               <UncontrolledCollapse toggler = "#toggle-horror">
 
-                  <p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>The Shining</p>
-                  <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Scratches</p>
-                  <p style = {{marginLeft: "10%", marginBottom: "2%"}}>The Hostile Hospital</p>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>The Shining<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>Scratches<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "2%"}}>The Hostile Hospital<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
 
 
               </UncontrolledCollapse>
-            <ListGroupItem tag = "a" href = "#" id = "toggle-SH">Self-Help<Badge pill style = {{marginLeft: "2%"}}>1</Badge></ListGroupItem>
+            <ListGroupItem tag = "a" href = "#" id = "toggle-SH">Self-Help<Badge pill style = {{marginLeft: "2%"}}>1</Badge><svg class="bi bi-trash" style = {{marginLeft: "50%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/>
+</svg></ListGroupItem>
               <UncontrolledCollapse toggler = "#toggle-SH">
 
-                  <p style = {{marginLeft: "10%", marginBottom: "2%", marginTop: "2%"}}>How To Be A Badass</p>
+                  <span><p style = {{marginLeft: "10%", marginBottom: "2%", marginTop: "2%"}}>How To Be A Badass<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
 
 
 
               </UncontrolledCollapse>
-            <ListGroupItem tag = "a" href = "#" id = "toggle-bio">Biography<Badge pill style = {{marginLeft: "2%"}}>5</Badge></ListGroupItem>
+            <ListGroupItem tag = "a" href = "#" id = "toggle-bio">Biography<Badge pill style = {{marginLeft: "2%"}}>5</Badge><svg class="bi bi-trash" style = {{marginLeft: "50%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/>
+</svg></ListGroupItem>
 
             <UncontrolledCollapse toggler = "#toggle-bio">
 
-                <p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>Slash: A Rockstars Guide to Life</p>
-                <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Some Book About Oprah</p>
-                <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Morgan Freeman Has A Cool Voice: Heres Why</p>
-                <p style = {{marginLeft: "10%", marginBottom: "0px"}}>Obama? Obama.</p>
-                <p style = {{marginLeft: "10%", marginBottom: "0px"}}>A Day In The Life of a Wendy's Employee</p>
+                <span><p style = {{marginLeft: "10%", marginBottom: "0px", marginTop: "2%"}}>Slash: A Rockstars Guide to Life<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>Some Book About Oprah<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>Morgan Freeman Has A Cool Voice: Heres Why<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>Obama? Obama.<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
+                <span><p style = {{marginLeft: "10%", marginBottom: "0px"}}>A Day In The Life of a Wendy's Employee<a href ="#" style = {{marginLeft: "2%"}}>Remove</a><svg class="bi bi-plus-circle" style = {{marginLeft: "2%"}} width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+<path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
+</svg></p></span>
 
             </UncontrolledCollapse>
 
